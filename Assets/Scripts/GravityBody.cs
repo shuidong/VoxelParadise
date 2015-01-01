ed09f21fc32efc01d0d9d62fc0c355e9d6fd5ade
William87137 <willem.gillis@gmail.com>
2015-01-01 14:48:28 +0100

    Firsts attempt at point driven gravity

---
+++ Assets/Scripts/GravityBody.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GravityBody : MonoBehaviour
+{
+
+	GravitySphere planet;
+
+	void Awake()
+	{
+		planet = GameObject.FindGameObjectWithTag("Planet").GetComponent<GravitySphere>();
+		GetComponent<Rigidbody>().useGravity = false;
+		GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+	}
+	void FixedUpdate()
+	{
+		planet.Attract(transform);
+		//Debug.Log(transform.position);
+	}
+}
---
+++ Assets/Scripts/GravitySphere.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravitySphere : MonoBehaviour {
+	public float gravity = -10f;
+
+	public void Attract(Transform body)
+	{
+		Vector3 gravityUp = (body.position - transform.position).normalized;
+		Vector3 localUp = body.up;
+		Debug.Log(-(gravityUp * gravity) +"    "+gravityUp);
+		body.GetComponent<Rigidbody>().AddForce(-(gravityUp * gravity));
+		body.rotation = Quaternion.FromToRotation(localUp, gravityUp) * body.rotation;
+		
+	}
+}
